10ab08bc4aa6a6828eb2fa73bc88b126ae45a89b
CGSRAILS <dbcurso@hotmail.com>
2012-09-10 07:19:04 -0300

    SUBIENDO TODO DESDE CERO

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lajuanita::Application.config.secret_token = '0560723764af97f0f88c5155050d723401b2e5fb0223ab7714760dd54714f50cd28db238e6688553bdbe2662631cc8a9e430c11e4661d15755aa920750abd956'
